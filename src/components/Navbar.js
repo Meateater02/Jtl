9374cace41e08bafb4ab1a19f7c42ecad5ded729
Meateater02 <dqh.phan@gmail.com>
2023-09-09 15:37:48 +1200

    WIP: hamburger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,27 @@
 import React from "react";
 import logo from "./Logo_Transparent.png";
 import "./Navbar.css";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="navigation">
-      <a href="/" className="brand-name">
-        Jane's Trading
-      </a>
-      <img src={logo}></img>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/products">Products</Link>
+      <div className="brand">
+        <a href="/" className="brand-name">
+          Jane's Trading
+        </a>
+        <img src={logo}></img>
+      </div>
+      <div className="hamburger">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div className="menu">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/products">Products</NavLink>
+      </div>
     </nav>
   );
 };
