28dd552c01e0e808e9d2c2dcea8bbe2c7d9ba8b7
Meateater02 <dqh.phan@gmail.com>
2023-09-11 20:24:03 +1200

    Done: hamburger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./Logo_Transparent.png";
 import "./Navbar.css";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [openHam, setHam] = useState(false);
+
   return (
     <nav className="navigation">
-      <div className="brand">
+      <Link to="/" className="company-name">
+        Jane's Trading
+      </Link>
+      <img src={logo}></img>
+      <div
+        className="hamburger"
+        onClick={() => {
+          setHam(!openHam);
+        }}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <ul className={openHam ? "open" : ""}>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">About</NavLink>
+        </li>
+        <li>
+          <NavLink to="/products">Products</NavLink>
+        </li>
+      </ul>
+
+      {/* <div className="brand">
         <a href="/" className="brand-name">
           Jane's Trading
         </a>
@@ -18,10 +46,10 @@ const Navbar = () => {
         <span></span>
       </div>
       <div className="menu">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/about">About</NavLink>
-        <NavLink to="/products">Products</NavLink>
-      </div>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/about">About</NavLink>
+      <NavLink to="/products">Products</NavLink>
+      </div> */}
     </nav>
   );
 };
