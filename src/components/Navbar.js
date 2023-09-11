348fbaba292f24424e10e6f66486cf9e49f04350
Meateater02 <dqh.phan@gmail.com>
2023-09-11 21:51:10 +1200

    WIP: fixing menu container

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,49 +7,36 @@ const Navbar = () => {
   const [openHam, setHam] = useState(false);
 
   return (
-    <nav className="navigation">
-      <Link to="/" className="company-name">
-        Jane's Trading
-      </Link>
-      <img src={logo}></img>
-      <div
-        className="hamburger"
-        onClick={() => {
-          setHam(!openHam);
-        }}
-      >
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-      <ul className={openHam ? "open" : ""}>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/about">About</NavLink>
-        </li>
-        <li>
-          <NavLink to="/products">Products</NavLink>
-        </li>
-      </ul>
-
-      {/* <div className="brand">
-        <a href="/" className="brand-name">
+    <nav>
+      <div className="logo-container">
+        <Link to="/" className="company-name">
           Jane's Trading
-        </a>
+        </Link>
         <img src={logo}></img>
+        <div
+          className="hamburger"
+          onClick={() => {
+            setHam(!openHam);
+          }}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </div>
-      <div className="hamburger">
-        <span></span>
-        <span></span>
-        <span></span>
+      <div className="menu-container">
+        <ul className={openHam ? "open" : ""}>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/products">Products</NavLink>
+          </li>
+        </ul>
       </div>
-      <div className="menu">
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/about">About</NavLink>
-      <NavLink to="/products">Products</NavLink>
-      </div> */}
     </nav>
   );
 };
